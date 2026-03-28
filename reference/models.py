2c48a0815346fe2f188d591bda849c6e196c868c
Yuriy Biryukov <yuiry@MacBook-Pro-Yuriy-2.local>
2026-03-28 08:52:50 +0200

    country.code: ISO Alpha-3 (max_length=3, unique)

--- reference/models.py
+++ reference/models.py
@@ -11,7 +11,7 @@ class Brand(models.Model):
 
 class Country(models.Model):
     name = models.CharField("Країна", max_length=255, unique=True)
-    code = models.CharField("Код", max_length=10, blank=True)
+    code = models.CharField("Код ISO Alpha-3", max_length=3, unique=True)
     is_active = models.BooleanField("Активна", default=True)
 
     def __str__(self):
