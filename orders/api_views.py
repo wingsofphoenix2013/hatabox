dab2fa08ebe389141fa265c5995e1ec837a68005
Yuriy Biryukov <yuiry@MacBook-Pro-Yuriy-2.local>
2026-03-29 08:54:06 +0300

    API: ExternalOrderPaymentStatus endpoint

--- orders/api_views.py
+++ orders/api_views.py
@@ -9,3 +9,6 @@ class ExternalOrderViewSet(ModelViewSet):
     queryset = ExternalOrder.objects.all()
     serializer_class = ExternalOrderSerializer
     permission_classes = [DjangoModelPermissions]
+
+    def perform_create(self, serializer):
+        serializer.save(created_by=self.request.user)
