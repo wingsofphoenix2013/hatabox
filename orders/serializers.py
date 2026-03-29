58a116461cedee99045ab097c247620607b06950
Yuriy Biryukov <yuiry@MacBook-Pro-Yuriy-2.local>
2026-03-29 08:26:18 +0300

    API: ExternalOrder readable fields

--- orders/serializers.py
+++ orders/serializers.py
@@ -3,6 +3,11 @@ from .models import ExternalOrder
 
 
 class ExternalOrderSerializer(serializers.ModelSerializer):
+    vendor_name = serializers.CharField(source="vendor.name", read_only=True)
+    status_name = serializers.CharField(source="status.name", read_only=True)
+    payment_status_name = serializers.CharField(source="payment_status.name", read_only=True)
+    created_by_username = serializers.CharField(source="created_by.username", read_only=True)
+
     class Meta:
         model = ExternalOrder
         fields = "__all__"
