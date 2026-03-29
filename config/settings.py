da8bde0ce65fed9a3664bb0e8ae0a6be8bdb19c4
Yuriy Biryukov <yuiry@MacBook-Pro-Yuriy-2.local>
2026-03-29 12:59:08 +0300

    Configure DRF session auth + CORS

--- config/settings.py
+++ config/settings.py
@@ -134,4 +134,14 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
 ]
 
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:5173",
+]
+
 CORS_ALLOW_CREDENTIALS = True
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.SessionAuthentication",
+    ],
+}
