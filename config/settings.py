2018e90129b45005a994828289883573c65f777f
Yuriy Biryukov <yuiry@MacBook-Pro-Yuriy-2.local>
2026-03-31 18:53:45 +0300

    Add pagination 50

--- config/settings.py
+++ config/settings.py
@@ -148,9 +148,7 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.SessionAuthentication",
     ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 25,
-    "PAGE_SIZE_QUERY_PARAM": "page_size",
-    "MAX_PAGE_SIZE": 100,
+    "PAGE_SIZE": 50,
 }
 
 SESSION_COOKIE_SAMESITE = "None"
