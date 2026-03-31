f0448e1382791cd7e124d2dfde7a937002be95a5
Yuriy Biryukov <yuiry@MacBook-Pro-Yuriy-2.local>
2026-03-31 22:23:33 +0300

    feat: add category filter to InvItemViewSet

--- inventory/api_views.py
+++ inventory/api_views.py
@@ -14,6 +14,15 @@ class InvItemCategoryViewSet(ReadOnlyModelViewSet):
     serializer_class = InvItemCategorySerializer
     
 class InvItemViewSet(ModelViewSet):
-    queryset = InvItem.objects.filter(is_active=True)
     serializer_class = InvItemSerializer
     permission_classes = [DjangoModelPermissions]
+
+    def get_queryset(self):
+        queryset = InvItem.objects.filter(is_active=True)
+
+        category = self.request.query_params.getlist("category")
+
+        if category:
+            queryset = queryset.filter(category_id__in=category)
+
+        return queryset
