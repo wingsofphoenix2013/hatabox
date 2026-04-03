a1990999e73142ebccf3b0603dfd0800038b84f8
Iurii Biriukov <wingsofphoenix.2013@gmail.com>
2026-04-03 19:52:15 +0300

    обновление API вендоров

--- vendors/api_views.py
+++ vendors/api_views.py
@@ -1,13 +1,13 @@
 from django.db import models
 
-from rest_framework.viewsets import ReadOnlyModelViewSet
+from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import DjangoModelPermissions
 
 from .models import Vendor
 from .serializers import VendorSerializer
 
 
-class VendorViewSet(ReadOnlyModelViewSet):
+class VendorViewSet(ModelViewSet):
     queryset = Vendor.objects.filter(is_active=True)
     serializer_class = VendorSerializer
     permission_classes = [DjangoModelPermissions]
