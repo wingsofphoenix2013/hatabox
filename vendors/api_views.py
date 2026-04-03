788876e602e09bb07e68a02e6ec9538986ea67fe
Iurii Biriukov <wingsofphoenix.2013@gmail.com>
2026-04-03 18:16:01 +0300

    включена возможность поиска

--- vendors/api_views.py
+++ vendors/api_views.py
@@ -1,3 +1,5 @@
+from django.db import models
+
 from rest_framework.viewsets import ReadOnlyModelViewSet
 from rest_framework.permissions import DjangoModelPermissions
 
@@ -9,3 +11,18 @@ class VendorViewSet(ReadOnlyModelViewSet):
     queryset = Vendor.objects.filter(is_active=True)
     serializer_class = VendorSerializer
     permission_classes = [DjangoModelPermissions]
+
+    def get_queryset(self):
+        queryset = self.queryset
+
+        search = self.request.query_params.get("search")
+        if search:
+            queryset = queryset.filter(
+                models.Q(code__icontains=search)
+                | models.Q(name__icontains=search)
+                | models.Q(legal_name__icontains=search)
+                | models.Q(phone__icontains=search)
+                | models.Q(email__icontains=search)
+            )
+
+        return queryset
