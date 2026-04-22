7fa282bafc9c1aad4bb7b8b642feca4cbb79e912
Iurii Biriukov <wingsofphoenix.2013@gmail.com>
2026-04-22 19:58:07 +0300

    Добавлено поле legal_name в list endpoint /api/organizations/

--- organizations/serializers.py
+++ organizations/serializers.py
@@ -14,6 +14,7 @@ class OrganizationListSerializer(serializers.ModelSerializer):
         fields = [
             "id",
             "name",
+            "legal_name",
             "type",
         ]
 
