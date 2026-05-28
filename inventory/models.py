5b4980f3a7b3705c2d2edf6cf87a40b3b55a456b
Iurii Biriukov <wingsofphoenix.2013@gmail.com>
2026-05-28 21:10:11 +0300

    Inventory: добавлен статус разработки продукта

--- inventory/models.py
+++ inventory/models.py
@@ -184,20 +184,6 @@ class Product(models.Model):
                 condition=models.Q(is_base_modification=True),
                 name="uq_product_family_single_base_modification",
             ),
-            models.CheckConstraint(
-                check=(
-                    models.Q(
-                        development_status="in_development",
-                        development_finished_at__isnull=True,
-                    )
-                    | models.Q(
-                        development_status="finished",
-                        development_finished_at__isnull=False,
-                        development_finished_at__gte=models.F("development_started_at"),
-                    )
-                ),
-                name="ck_product_development_status_dates",
-            ),
         ]
 
     def __str__(self):
